921478557bcf07ad464a4645d635a2fb4b0088af
“Kapil <kapil.manwani@bt.com>
2025-09-20 14:53:17 +0530

    created-lb-target-group

--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ module "jenkins" {
 }
 
 
-# module "lb_target_group" {
-#   source                   = "./load-balancer-target-group"
-#   lb_target_group_name     = "jenkins-lb-target-group"
-#   lb_target_group_port     = 8080
-#   lb_target_group_protocol = "HTTP"
-#   vpc_id                   = module.networking.dev_proj_1_vpc_id
-#   ec2_instance_id          = module.jenkins.jenkins_ec2_instance_ip
-# }
+module "lb_target_group" {
+  source                   = "./load-balancer-target-group"
+  lb_target_group_name     = "jenkins-lb-target-group"
+  lb_target_group_port     = 8080
+  lb_target_group_protocol = "HTTP"
+  vpc_id                   = module.networking.dev_proj_1_vpc_id
+  ec2_instance_id          = module.jenkins.jenkins_ec2_instance_ip
+}
 
 # module "alb" {
 #   source                    = "./load-balancer"

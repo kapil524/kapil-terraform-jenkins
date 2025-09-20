2e596c0130e592c05b3fd714762e73b66eef0931
“Kapil <kapil.manwani@bt.com>
2025-09-20 12:57:07 +0530

    added-internet-gateway

--- networking/main.tf
+++ networking/main.tf
@@ -55,16 +55,16 @@ resource "aws_subnet" "dev_proj_1_private_subnets" {
   }
 }
 
-# ###########################
-# # Internet Gateway
-# ###########################
+###########################
+# Internet Gateway
+###########################
 
-# resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
-#   vpc_id = aws_vpc.dev_proj_1_vpc_ap_south_1.id
-#   tags = {
-#     Name = "dev-proj-1-igw"
-#   }
-# }
+resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
+  vpc_id = aws_vpc.dev_proj_1_vpc_ap_south_1.id
+  tags = {
+    Name = "dev-proj-1-igw"
+  }
+}
 
 # ###########################
 # # Public Route Table
